6c1ad639b3675ef078e36ed82b775b8ec301ba5a
Saikat048 <mdsaikathosan048@gmail.com>
2022-04-29 00:26:15 +0600

    create components header customlink

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <CustomLink to='/home'>Home</CustomLink>
+                            <CustomLink to='/login'>Log in</CustomLink> 
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
