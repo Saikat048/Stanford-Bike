e3e2a9409f8e55a0288c248ee21a56ccfa110431
Saikat048 <mdsaikathosan048@gmail.com>
2022-04-29 15:44:20 +0600

    commit authentication sign up

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Signup.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { sendEmailVerification } from 'firebase/auth';
 
 const Signup = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState(''); 
+    const [error, setError] = useState(''); 
+    const navigate = useNavigate(); 
+
+ 
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value);
+    }
+    const [createUserWithEmailAndPassword, user, loading] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        if(password !== confirmPassword){
+            setError('Password did not match')
+            return;
+        }
+        if (password.length <6){
+            setError('Password must be at least 6 characters or more...')
+            return;
+        }
+        createUserWithEmailAndPassword(email, password)
+         // emailVerification
+         sendEmailVerification();
+    }
+
+    if (user) {
+        navigate('/home')
+    }
+
     return (
         <div>
            <div className='container'>
@@ -12,17 +55,18 @@ const Signup = () => {
                     <img className='img-fluid  img' src="https://www.seoclerk.com/pics/want52650-19fSlK1499512787.jpg" alt="" />
                 </div>
                 <div className=' col-md-7 col-lg-7'>
-                    <Form className='mt-5 form form-control'>
+                    <Form onSubmit={handleSubmit}  className='mt-5 form form-control'>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" required />
+                            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" required />
+                            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
                             <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control type="password" placeholder="Confirm Password" required />
+                            <Form.Control onBlur={handleConfirmPasswordBlur} type="password" placeholder="Confirm Password" required />
+                            <p className='text-danger'>{error}</p>
                         </Form.Group> <p>Already have an account? <Link to="/login">Please Log In</Link></p> 
                         <Button variant="primary" type="submit">
                             Sign Up
---
+++ src/firebase.init.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh1LIuv5IeQme45kXq_Lhuk4oGc8DeMtE",
+  authDomain: "assignment-11-894b0.firebaseapp.com",
+  projectId: "assignment-11-894b0",
+  storageBucket: "assignment-11-894b0.appspot.com",
+  messagingSenderId: "263323467004",
+  appId: "1:263323467004:web:600cc0f22ebfd090212b79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app); 
+
+export default auth;
