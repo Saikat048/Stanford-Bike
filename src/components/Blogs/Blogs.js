86412fb23bd4631c769e649e9a1117ae69cbc1da
Saikat048 <mdsaikathosan048@gmail.com>
2022-05-02 05:15:03 +0600

    commit deliverd

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
-import './Blogs.css'
+import './Blogs.css';
 
 const Blogs = () => {
     return (
--- src/components/ShowDetail/ShowDetail.js
+++ src/components/ShowDetail/ShowDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import './ShowDetail.css'
 
@@ -19,25 +19,61 @@ const ShowDetail = () => {
 
     }, [])
 
+    const [quantity, setQuantity] = useState([]);
+
+    const delivered = () => {
+        if (quantity <= 0) {
+            alert('no product');
+            return;
+        }
+        const productDelivered = (quantity - 1);
+        setQuantity(productDelivered);
+    }
+
+ 
+    const handleSubmit = event => {
+        event.preventDefault();
+        const number = event.target.text.value;
+        if (number <= 0) {
+            alert('Not A valid Number');
+            event.target.reset(); 
+            return;
+        }
+        const addToNumber = quantity + parseInt(number);
+        setQuantity(addToNumber); 
+        event.target.reset(); 
+    }
+ 
+
     return (
         <div>
             <div className='container detail'>
                 <div className="row justify-content-between">
-                    <Card className='col-6 mx-auto' style={{ width: '18rem' }}>
+                    <Card className='col-6 mx-auto' style={{ width: '20rem' }}>
                         <Card.Img variant="top" src={product.img} />
-                        <Card.Body>
-                            <Card.Title>Name : {product.name}</Card.Title>
-                            <Card.Text>
-                                {product.description}
-                            </Card.Text>
+
+
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3 mt-5" controlId="formBasicEmail">
+                                <Form.Control type="number" placeholder="Add Quantity" name='text' required/>
+                            </Form.Group> 
+                           <Button variant="primary" type="submit">Add</Button>
+                        <Button className='ms-3 ' onClick={delivered} variant="primary">Delivered</Button> 
+                        </Form>
+                    </Card>
+
+
+                    <div className='bg-danger col-6 mx-auto'>
+                        <h1 className='text-center'>Information</h1>
+                        <hr className='w-50 mx-auto' />
+
+                        <div className='container mt-5'>
+                            <h3>Name : {product.name}</h3>
+                            <p>Description : {product.description}</p>
                             <p>Price : {product.price}</p>
                             <p>Quantity : {product.quantity}</p>
                             <p>Supplier Name : {product.supplier}</p>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                    <div className='bg-danger col-6 mx-auto'>
-                        <h1>Information</h1>
+                        </div>
                     </div>
                 </div>
             </div>
